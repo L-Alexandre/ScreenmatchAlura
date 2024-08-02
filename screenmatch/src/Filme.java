8842f8ed2e615d0e32e88982aaef1bfb7654f2be
l-alexandre <luiz-alexandre@outlook.com.br>
2024-08-01 23:33:51 -0300

    Classe Filme e Principal para avaliação de filmes.

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,22 @@
+public class Filme {
+	String nome;
+	int anoDeLancamento;
+	int totalDeAvaliacoes;
+	int duracaoEmMinutos;
+	boolean incluidoNoPlano;
+	double somaDasavaliacoes;
+
+	void exibeFichatecnica() {
+		System.out.println("Nome do Filme:" + nome);
+		System.out.println("Anoe de Lançamento: " + anoDeLancamento);
+	}
+
+	void avalia(double nota) {
+		somaDasavaliacoes += nota;
+		totalDeAvaliacoes++;
+	}
+	
+	double pegaMedia(){
+		return somaDasavaliacoes / totalDeAvaliacoes;
+	}
+}
